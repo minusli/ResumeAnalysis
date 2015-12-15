32c7b16567d3f07e887eb976aa67012372d546f2
ChrisLee <657143946@qq.com>
2015-12-15 17:08:27 +0800

    每次查询resume mongo 关闭链接

--- keywords/extract_tags.py
+++ keywords/extract_tags.py
@@ -10,4 +10,6 @@ from Main import settings
 
 
 def find_resume_by_id(cv_id, source):
-    return settings.MongoConf.resume_collection.find_one({"cv_id": cv_id, "source": source})
+    data = settings.MongoConf.resume_collection.find_one({"cv_id": cv_id, "source": source})
+    settings.MongoConf.resume_mongo.close()
+    return data
